561b1b264a1950b5ad0f38c8e70fbc683e9e1d09
60342360047-1 <naiiwolf00@gmail.com>
2023-05-25 16:38:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: '147.50.231.19',
+    user: 'devsriwa_test',
+    password:'*Test1234*',
+    database: 'devsriwa_test'
+  });
+
+app.use(cors())
+app.use(express.json());
+
+
+app.get('/users', function (req, res, next) {
+
+    const q = "SELECT * FROM `test`"
+
+    connection.query(q, (err,data)=>{
+        res.json(data)
+    })
+
+
+})
+
+app.post('/user', function (req, res, next) {
+    const name = req.body.name
+    const q = "INSERT INTO `test` (name) VALUE (?)"
+
+    connection.query(q, [name], (err,data)=>{
+        if(err) return res.json(err)
+        res.json(data)
+    })
+
+
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
